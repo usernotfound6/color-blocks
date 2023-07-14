02a1f29b0e61206e6f3b038e19a8cb1b0bbdaa13
Fraol Ayehu <phrolaye@gmail.com>
2023-07-13 22:34:59 -0500

    blocks and deletion done

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('🟥 🟦 🟩 🟨');
+// console.log('🟥 🟦 🟩 🟨');
+$(document).ready(onReady);
+
+function onReady() {
+
+     // Listeners / Handlers
+  $('#red-btn').on("click", makeRedblock) 
+  $('#blue-btn').on("click", makeBlueblock) 
+  $('#green-btn').on("click", makeGreenblock) 
+  $('#yellow-btn').on("click", makeYellowblock) 
+
+$('#blocks').on('click', '.block.red-fill', deleteRedblock)
+$('#blocks').on('click', '.block.blue-fill', deleteBlueblock)
+$('#blocks').on('click', '.block.green-fill', deleteGreenblock)
+$('#blocks').on('click', '.block.yellow-fill', deleteYellowblock)
+}
+
+function makeRedblock() {
+    console.log("🟥")
+    $('#blocks').append(`
+      <div class="block red-fill"></div>
+    `);
+  }
+
+  function makeBlueblock() {
+    console.log("🟦")
+    $('#blocks').append(`
+      <div class="block blue-fill"></div>
+    `);
+  }
+
+  function makeGreenblock() {
+    console.log("🟩")
+    $('#blocks').append(`
+      <div class="block green-fill"></div>
+    `);
+  }
+
+  function makeYellowblock() {
+    console.log("🟨")
+    $('#blocks').append(`
+      <div class="block yellow-fill"></div>
+    `);
+  }
+
+  function deleteRedblock() {
+    $(this).remove()
+    console.log($(this))
+  }
+
+  function deleteBlueblock() {
+    $(this).remove()
+    console.log($(this))
+  }
+  function deleteGreenblock() {
+    $(this).remove()
+    console.log($(this))
+  }
+
+  function deleteYellowblock() {
+    $(this).remove()
+    console.log($(this))
+  }
